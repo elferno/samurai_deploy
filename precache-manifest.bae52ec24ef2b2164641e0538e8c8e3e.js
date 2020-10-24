47a9aa35c0d089d0e2665ff733fcacb561ecfb3b
elferno <elferno@inbox.ru>
2020-10-24 11:31:50 +0500

    Updates

--- precache-manifest.bae52ec24ef2b2164641e0538e8c8e3e.js
+++ precache-manifest.bae52ec24ef2b2164641e0538e8c8e3e.js
@@ -1,59 +1,59 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bcdd78b6773bc41bd9d6a19e5d65c186",
+    "revision": "e91dcc3768fcb111abed2aef9812d77f",
     "url": "/samurai_deploy/index.html"
   },
   {
-    "revision": "a68b45930a2b370532ef",
+    "revision": "a1035be292e6dc471787",
     "url": "/samurai_deploy/static/css/3.88dbfda5.chunk.css"
   },
   {
-    "revision": "6cf754ef0ebb643e8d34",
+    "revision": "0685eab4af909e0004fd",
     "url": "/samurai_deploy/static/css/4.ae853573.chunk.css"
   },
   {
-    "revision": "0e8ec18680c536317359",
+    "revision": "cae1c43ed9c9f966d5db",
     "url": "/samurai_deploy/static/css/5.775d1f91.chunk.css"
   },
   {
-    "revision": "481b49d41f10e6be4b30",
+    "revision": "acdec104b29430739ef7",
     "url": "/samurai_deploy/static/css/6.7327e77c.chunk.css"
   },
   {
-    "revision": "fbe3bc80448cf71bd292",
+    "revision": "6c74f29dfec7ce67acbe",
     "url": "/samurai_deploy/static/css/main.adcd09ca.chunk.css"
   },
   {
-    "revision": "6aafee5966924156023b",
-    "url": "/samurai_deploy/static/js/2.5fc91cc2.chunk.js"
+    "revision": "e5fb8ca4c79767454ace",
+    "url": "/samurai_deploy/static/js/2.66de9027.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/samurai_deploy/static/js/2.5fc91cc2.chunk.js.LICENSE.txt"
+    "url": "/samurai_deploy/static/js/2.66de9027.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a68b45930a2b370532ef",
-    "url": "/samurai_deploy/static/js/3.0020d31f.chunk.js"
+    "revision": "a1035be292e6dc471787",
+    "url": "/samurai_deploy/static/js/3.734c91fd.chunk.js"
   },
   {
-    "revision": "6cf754ef0ebb643e8d34",
-    "url": "/samurai_deploy/static/js/4.d5157359.chunk.js"
+    "revision": "0685eab4af909e0004fd",
+    "url": "/samurai_deploy/static/js/4.50357c17.chunk.js"
   },
   {
-    "revision": "0e8ec18680c536317359",
-    "url": "/samurai_deploy/static/js/5.c852a177.chunk.js"
+    "revision": "cae1c43ed9c9f966d5db",
+    "url": "/samurai_deploy/static/js/5.3acf1ff5.chunk.js"
   },
   {
-    "revision": "481b49d41f10e6be4b30",
-    "url": "/samurai_deploy/static/js/6.245d23f5.chunk.js"
+    "revision": "acdec104b29430739ef7",
+    "url": "/samurai_deploy/static/js/6.c2a53118.chunk.js"
   },
   {
-    "revision": "fbe3bc80448cf71bd292",
-    "url": "/samurai_deploy/static/js/main.04903c6a.chunk.js"
+    "revision": "6c74f29dfec7ce67acbe",
+    "url": "/samurai_deploy/static/js/main.a68742fc.chunk.js"
   },
   {
-    "revision": "53624e236cb0f3cb3f83",
-    "url": "/samurai_deploy/static/js/runtime-main.95fc4d77.js"
+    "revision": "facba539779f09b3d7e1",
+    "url": "/samurai_deploy/static/js/runtime-main.1758858b.js"
   },
   {
     "revision": "7386951995e51d845253996e909b8b0c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samurai_deploy/precache-manifest.6a21e107063824b50f4c6e2090d946b0.js"
+  "/samurai_deploy/precache-manifest.bae52ec24ef2b2164641e0538e8c8e3e.js"
 );
 
 self.addEventListener('message', (event) => {
